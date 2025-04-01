858c92810041fd88b5e54619d69208d0e0b9831f
Budaragina <angelinabudaragina04@mail.ru>
2025-04-01 14:43:29 +0300

    CLOSE #3: Design Import and Restore

--- Admin/AdminImportAndRestore.Designer.cs
+++ Admin/AdminImportAndRestore.Designer.cs
@@ -46,7 +46,7 @@ namespace MagazinTechniki
             this.buttonImport.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.buttonImport.Font = new System.Drawing.Font("Book Antiqua", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.buttonImport.ForeColor = System.Drawing.Color.Black;
-            this.buttonImport.Location = new System.Drawing.Point(305, 228);
+            this.buttonImport.Location = new System.Drawing.Point(305, 229);
             this.buttonImport.Name = "buttonImport";
             this.buttonImport.Size = new System.Drawing.Size(156, 43);
             this.buttonImport.TabIndex = 95;
